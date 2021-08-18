b8a00480b06e0afe843878c780d50830096e3b25
Hristo-Cholakov <Hristo.Cholakov@a1.bg>
2021-08-18 16:53:19 +0300

    Add Navbar and WIFI string

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 function codeGenerator(){
-    var urlString = document.getElementById("url").value;
-    new QRCode(document.getElementById("qr"), urlString);
-    
+    // var urlString = document.getElementById("url").value;
+    var ssid = document.getElementById("ssid").value;
+console.log(ssid);
+
+var encr = document.getElementById("encr").value;
+
+console.log(encr);
+
+var key = document.getElementById("pass").value;
+    // new QRCode(document.getElementById("qr"), "WIFI:S:home.cweiske.de;T:WPA;P:2342;;");
+    new QRCode(document.getElementById("qr"), "WIFI:S:" + ssid + ";T:" + encr + ";P:" + key + ";;");
       
   }
