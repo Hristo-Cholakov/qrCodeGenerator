eb6f78c6d0484130aacdb2d1bc1b40f81acf2a07
Hristo-Cholakov <Hristo.Cholakov@a1.bg>
2021-08-19 17:29:26 +0300

    Fix font awesome CDN and add password toggler

--- app.js
+++ app.js
@@ -11,4 +11,13 @@ var key = document.getElementById("pass").value;
     // new QRCode(document.getElementById("qr"), "WIFI:S:home.cweiske.de;T:WPA;P:2342;;");
     new QRCode(document.getElementById("qr"), "WIFI:S:" + ssid + ";T:" + encr + ";P:" + key + ";;");
       
+  };
+
+  function toggleVisibility(){
+      var toggler = document.getElementById("pass");
+      if(toggler.type === "password"){
+          toggler.type = "text"
+      }else {
+          toggler.type = "password"
+      }
   }
