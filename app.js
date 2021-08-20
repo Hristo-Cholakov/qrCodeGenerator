cfbf09d40e22122ef6097e1eac6b4079e1546c2b
Hristo-Cholakov <Hristo.Cholakov@a1.bg>
2021-08-20 12:22:53 +0300

    Add Hidden network option, Create new URL skeleton page

--- app.js
+++ app.js
@@ -1,28 +1,24 @@
 function codeGenerator(){
-    // var urlString = document.getElementById("url").value;
+// var urlString = document.getElementById("url").value;
     var ssid = document.getElementById("ssid").value;
-console.log(ssid);
+    var encr = document.getElementById("encr").value;
+    var hiddenCheck = document.getElementById("hidden").checked;
+    var key = document.getElementById("pass").value;
+// new QRCode(document.getElementById("qr"), "WIFI:S:home.cweiske.de;T:WPA;P:2342;H:true;");
+    var canvas = new QRCode(document.getElementById("qr"), "WIFI:S:" + ssid + ";T:" + encr + ";P:" + key + ";" + "H:" + hiddenCheck + ";;");
 
-var encr = document.getElementById("encr").value;
+};
 
-console.log(encr);
-
-var key = document.getElementById("pass").value;
-    // new QRCode(document.getElementById("qr"), "WIFI:S:home.cweiske.de;T:WPA;P:2342;;");
-    new QRCode(document.getElementById("qr"), "WIFI:S:" + ssid + ";T:" + encr + ";P:" + key + ";;");
-      
-  };
-
-  function toggleVisibility(){
-      var toggler = document.getElementById("pass");
-      var slasher = document.getElementById("slasher");
-      if(toggler.type === "password"){
-          toggler.type = "text"
-          slasher.classList.add("fa-eye-slash");
-          slasher.classList.remove("fa-eye");
-      }else {
-          toggler.type = "password";
-          slasher.classList.remove("fa-eye-slash");
-          slasher.classList.add("fa-eye");
-      }
-  }
+function toggleVisibility(){
+    var toggler = document.getElementById("pass");
+    var slasher = document.getElementById("slasher");
+        if(toggler.type === "password"){
+            toggler.type = "text"
+            slasher.classList.add("fa-eye-slash");
+            slasher.classList.remove("fa-eye");
+        }else {
+            toggler.type = "password";
+            slasher.classList.remove("fa-eye-slash");
+            slasher.classList.add("fa-eye");
+        }
+    };
